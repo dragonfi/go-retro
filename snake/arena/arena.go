344f0b2625b8f7ff44fc27f2e439540a9e57d4da
dragonfi <david.gabor.bodor@gmail.com>
2014-01-08 12:24:11 +0100

    snake moves.

--- snake/arena/arena.go
+++ snake/arena/arena.go
@@ -1,8 +1,9 @@
 package arena
 
-type Arena interface{
+type Arena interface {
 	State() State
 	Tick()
+	SetSnakeHeading(h Direction)
 }
 
 type Direction int
@@ -19,16 +20,16 @@ type Position struct {
 }
 
 type State struct {
-	Size Position
+	Size  Position
 	Snake Snake
 }
 
 type Snake struct {
 	Segments []Position
-	Heading Direction
+	Heading  Direction
 }
 
-func (s Snake) Head() Position{
+func (s Snake) Head() Position {
 	return s.Segments[0]
 }
 
@@ -36,7 +37,7 @@ func (s Snake) Length() int {
 	return len(s.Segments)
 }
 
-func (s* Snake) move() {
+func (s *Snake) moveHead() {
 	switch s.Heading {
 	case EAST:
 		s.Segments[0].X += 1
@@ -49,32 +50,47 @@ func (s* Snake) move() {
 	}
 }
 
+func (s *Snake) moveBody() {
+	for i := len(s.Segments) - 1; i > 0; i-- {
+		s.Segments[i] = s.Segments[i-1]
+	}
+}
+
+func (s *Snake) move() {
+	s.moveBody()
+	s.moveHead()
+}
+
 type arena struct {
-	size Position
+	size  Position
 	snake Snake
 }
 
 func (a arena) State() State {
 	segments := make([]Position, len(a.snake.Segments))
 	copy(segments, a.snake.Segments)
-	return State{Size:a.size, Snake:Snake{Segments:segments, Heading: a.snake.Heading}}
+	return State{Size: a.size, Snake: Snake{Segments: segments, Heading: a.snake.Heading}}
 }
 
-func (a* arena) Tick() {
+func (a *arena) Tick() {
 	a.snake.move()
+}
+
+func (a *arena) SetSnakeHeading(h Direction) {
+	a.snake.Heading = h
 }
 
 func newSnake(x, y int, heading Direction, size int) Snake {
 	segments := make([]Position, size, size*10)
-	s := Snake{Segments:segments}
+	s := Snake{Segments: segments}
 	for i := 0; i < size; i++ {
-		s.Segments[i] = Position{x-i, y}
+		s.Segments[i] = Position{x - i, y}
 	}
 	return s
 }
 
-func New(width, height int) Arena{
+func New(width, height int) Arena {
 	s := newSnake(width/2, height/2, EAST, 5)
-	a := arena{size:Position{width, height}, snake:s}
+	a := arena{size: Position{width, height}, snake: s}
 	return &a
 }
--- snake/arena/arena_test.go
+++ snake/arena/arena_test.go
@@ -1,8 +1,8 @@
 package arena_test
 
 import (
-	"testing"
 	"../arena"
+	"testing"
 )
 
 func TestArenaCreation(t *testing.T) {
@@ -30,43 +30,55 @@ func makeArena(t *testing.T, width, height int) arena.Arena {
 	return a
 }
 
-func testSnakeState(t *testing.T, initial arena.Snake, direction arena.Direction, s arena.Snake) {
+func testSnakeMovementHead(t *testing.T, initial arena.Snake, direction arena.Direction, s arena.Snake) {
 	initial_head := initial.Head()
+	h := s.Head()
+	var dx, dy int
+	switch direction {
+	case arena.EAST:
+		dx = 1
+	case arena.NORTH:
+		dy = -1
+	case arena.WEST:
+		dx = -1
+	case arena.SOUTH:
+		dy = 1
+	}
+	if h.X != initial_head.X+dx || h.Y != initial_head.Y+dy {
+		t.Error("Wrong position for snake head:", h.X, h.Y, "Expected:", initial_head.X+dx, initial_head.Y+dy)
+		t.Error("Note: Direction:", direction)
+	}
+}
+
+func testSnakeMovementBody(t *testing.T, initial, s arena.Snake) {
+	for i := 1; i < len(s.Segments); i++ {
+		if s.Segments[i] != initial.Segments[i-1] {
+			t.Error("Wrong segment at position:", i, "segment:", s.Segments[i], "expected:", initial.Segments[i-1])
+		}
+	}
+}
+
+func testSnakeMovement(t *testing.T, a arena.Arena, direction arena.Direction) {
+	initial := a.State().Snake
+	a.SetSnakeHeading(direction)
+	a.Tick()
+	s := a.State().Snake
 	if s.Heading != direction {
 		t.Error("Wrong direction!")
 	}
 	if s.Length() != 5 || len(s.Segments) != 5 {
 		t.Error("Wrong snake size: Expected:", 5, "Got:", s.Length())
 	}
-	h := s.Head()
-	if h.X != initial_head.X + 1 || h.Y != initial_head.Y {
-		t.Error("Wrong position for snake head:", h.X, h.Y, "Expected:", initial_head.X+1, initial_head.Y)
-		t.Error("Note: Direction:", direction)
-	}
+	testSnakeMovementHead(t, initial, direction, s)
+	testSnakeMovementBody(t, initial, s)
 }
 
-/*
 func TestSnakeMovement(t *testing.T) {
 	const width = 40
 	const height = 20
 	a := makeArena(t, width, height)
-	var initial = a
-	initial_snake := a.Snake
-	t.Log( initial_snake.Segment[0].X)
-	t.Log( initial_snake.Head())
-
-	a.Tick()
-	t.Log(initial)
-	t.Log(a)
-	t.Log(initial_snake.Head())
-
-	testSnakeState(t, initial_snake, arena.EAST, a.Snake)
-
-
-	initial_snake = a.Snake
-	a.Snake.Heading = arena.NORTH
-	a.Tick()
-	testSnakeState(t, initial_snake, arena.NORTH, a.Snake)
+	testSnakeMovement(t, a, arena.EAST)
+	testSnakeMovement(t, a, arena.NORTH)
+	testSnakeMovement(t, a, arena.WEST)
+	testSnakeMovement(t, a, arena.SOUTH)
 }
-*/
-
